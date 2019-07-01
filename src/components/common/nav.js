def605ed3d0373d0b1018209137b96908d229f1b
Jordan <jordanwalker@Jordans-MacBook-Pro.local>
2019-06-30 23:03:02 -0500

    Added active links to nav

--- src/components/common/nav.js
+++ src/components/common/nav.js
@@ -6,19 +6,29 @@ const Nav = () => (
   <NavWrapper>
     <NavList>
       <NavListItem>
-        <StyledNavLink to="/">Home</StyledNavLink>
+        <StyledNavLink to="/" activeStyle={activeStyles}>
+          Home
+        </StyledNavLink>
       </NavListItem>
       <NavListItem>
-        <StyledNavLink to="/about">About</StyledNavLink>
+        <StyledNavLink to="/about" activeStyle={activeStyles}>
+          About
+        </StyledNavLink>
       </NavListItem>
       <NavListItem>
-        <StyledNavLink to="/services">Services</StyledNavLink>
+        <StyledNavLink to="/services" activeStyle={activeStyles}>
+          Services
+        </StyledNavLink>
       </NavListItem>
       <NavListItem>
-        <StyledNavLink to="/cars">Cars</StyledNavLink>
+        <StyledNavLink to="/cars" activeStyle={activeStyles}>
+          Cars
+        </StyledNavLink>
       </NavListItem>
       <NavListItem>
-        <StyledNavLink to="/contact">Contact</StyledNavLink>
+        <StyledNavLink to="/contact" activeStyle={activeStyles}>
+          Contact
+        </StyledNavLink>
       </NavListItem>
     </NavList>
   </NavWrapper>
@@ -40,18 +50,31 @@ const NavList = styled.ul`
   display: flex;
   height: 100%;
   align-items: center;
-  padding: 16px;
+  margin: 0px 16px;
+  border-left: 1px solid black;
   box-sizing: border-box;
 `;
 
 const NavListItem = styled.li`
-  padding-right: 48px;
-  margin-bottom: 0px;
+  height: 100%;
+  display: flex;
+  border-right: 1px solid black;
+  margin: 0;
 `;
 
 const StyledNavLink = styled(props => <Link {...props} />)`
   font-size: 18px;
+  width: 100%;
   color: white;
   text-decoration: none;
   text-transform: uppercase;
+  display: flex;
+  align-items: center;
+  padding: 0 24px;
+  border-bottom: 2px solid red;
 `;
+
+// This object is passed a props to the link component when page matches nav.
+const activeStyles = {
+  borderBottom: "4px solid white",
+};
